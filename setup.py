a4cb6b76a1231f9b114a3e8de3ffec324e2970ea
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-04-13 09:09:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@
 from setuptools import setup
 
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="nettigo_air_monitor",
     version="0.2.3",
     author="Maciej Bieniek",
     description="Python wrapper for getting air quality data from Nettigo Air Monitor devices.",
-    long_description = file: README.md,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     include_package_data=True,
     url="https://github.com/bieniu/nettigo-air-monitor",
