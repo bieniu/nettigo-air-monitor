c359450644ed819f01156336a81329b014c64126
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-04-27 13:21:06 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="nettigo_air_monitor",
-    version="0.2.3",
+    version="0.2.4",
     author="Maciej Bieniek",
     description="Python wrapper for getting air quality data from Nettigo Air Monitor devices.",
     long_description=long_description,
