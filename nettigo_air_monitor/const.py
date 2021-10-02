acf692a3ea74dd281ce2c737e38784adc27df625
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-10-02 15:55:43 +0200

    Python 3.10 (#4)
    * Add support for Python 3.10
    
    * Disable consider-alternative-union-syntax

--- nettigo_air_monitor/const.py
+++ nettigo_air_monitor/const.py
@@ -1,20 +1,22 @@
 """Constants for nettigo-air-monitor library."""
-from typing import Dict
+from __future__ import annotations
 
-ATTR_DATA: str = "data"
-ATTR_SENSOR_VALUES: str = "sensordatavalues"
-ATTR_UPTIME = "uptime"
-ATTR_VALUES: str = "values"
+from typing import Final
 
-RETRIES: int = 4
-TIMEOUT: int = 5
+ATTR_DATA: Final[str] = "data"
+ATTR_SENSOR_VALUES: Final[str] = "sensordatavalues"
+ATTR_UPTIME: Final[str] = "uptime"
+ATTR_VALUES: Final[str] = "values"
 
-ENDPOINTS: Dict[str, str] = {
+RETRIES: Final[int] = 4
+TIMEOUT: Final[int] = 5
+
+ENDPOINTS: Final[dict[str, str]] = {
     ATTR_DATA: "http://{host}/data.json",
     ATTR_VALUES: "http://{host}/values",
 }
 
-RENAME_KEY_MAP = [
+RENAME_KEY_MAP: Final[list[tuple[str, str]]] = [
     ("conc_co2_ppm", "mhz14a_carbon_dioxide"),
     ("humidity", "dht22_humidity"),
     ("sds_p1", "sds011_p1"),
@@ -22,4 +24,4 @@ RENAME_KEY_MAP = [
     ("temperature", "dht22_temperature"),
 ]
 
-MAC_PATTERN: str = r"([0-9a-fA-F]{2}[:]){5}([0-9a-fA-F]{2})"
+MAC_PATTERN: Final[str] = r"([0-9a-fA-F]{2}[:]){5}([0-9a-fA-F]{2})"
