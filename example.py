542e6c12d14291a4c369765426030102556c4b8e
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-11-05 14:55:15 +0100

    Add pre-commit (#6)
    * Update VSCode settings
    
    * Update requirements
    
    * Add pre-commit
    
    * Update CI
    
    * Update requirements-test
    
    * Fix pre-commit errors

--- example.py
+++ example.py
@@ -15,7 +15,7 @@ async def main():
     try:
         async with ClientSession() as websession, async_timeout.timeout(30):
             nam = NettigoAirMonitor(websession, HOST)
-            
+
             data = await nam.async_update()
             mac = await nam.async_get_mac_address()
     except (
